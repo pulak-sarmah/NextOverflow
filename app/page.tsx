5224e08f19f178b773ae94fb263014b8cb2008da
pulak-sarmah <pulaksarmah333@gmail.com>
2023-11-03 21:35:12 +0530

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">hello next js 13</h1>
-      <h1 className="h2-bold">hello next js 13</h1>
-      <h1 className="h3-bold">hello next js 13</h1>
-    </div>
-  );
-}
